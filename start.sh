5b55d7aa1f3c7311ab50b201dfe06af33a2ee55a
Michael Clayton <michael.clayton@procore.com>
2021-07-13 20:05:16 -0700

    Minor cleanup

--- start.sh
+++ start.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
-sandbox() {
-  echo "Setting up dockerized sandbox environment..."
+# Prints path to directory containing this script
+function scriptDirectory {
+  local self_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  echo "$self_dir"
+}
+
+# Bring in constants
+source "$(scriptDirectory)/installation/constants.sh"
+# Bring in the helper functions
+source "$(scriptDirectory)/installation/helperFunctions.sh"
+
+sandboxInstall() {
+  info "Setting up dockerized sandbox environment..."
   docker build -t macsetup .
-  echo "Built docker image for sandbox environment."
-  echo "Starting sandbox environment..."
+  success "Built docker image for sandbox environment."
+  info "Starting sandbox environment..."
   docker container run --rm -it -h sandbox macsetup /bin/bash -c "./install.sh"
 }
 
-function machine() {
+function machineInstall() {
   echo "Using current machine environment..."
   ./install.sh
 }
 
 ### Main ###
-
-
-echo "ⓘ Sandbox Environment: Installation/Setup changes are made in a dockerized sandbox environment and will not effect the current actual machine."
-echo "ⓘ Current Machine: Installation/Setup changes made in this environment will modify and effect the current actual machine."
-echo ""
+echo
+echo " =============================================================================================================================================="
+echo "| Sandbox Environment: Installation/Setup changes are made in a dockerized sandbox environment and will not effect the current actual machine. |"
+echo "| Current Machine: Installation/Setup changes made in this environment will modify and effect the current actual machine.                      |"
+echo " =============================================================================================================================================="
+echo
 
 PS3='   => Choose Execution Environment: '
 options=("Sandbox (Docker)" "Current Machine" "Quit")
@@ -26,16 +38,16 @@ select opt in "${options[@]}"
 do
     case $opt in
         "Sandbox (Docker)")
-            sandbox
+            sandboxInstall
             break
             ;;
         "Current Machine")
-            machine
+            machineInstall
             break
             ;;
         "Quit")
             break
             ;;
-        *) echo "Invalid option $REPLY";;
+        *) warn "Invalid option $REPLY";;
     esac
 done
