a244f38d335fc0c2173d5a0622a49e8f240e0331
Michael Clayton <michael.clayton@cadencerpm.com>
2021-12-12 19:47:41 -0800

    Fix font paths

--- sections/setup_fonts.sh
+++ sections/setup_fonts.sh
@@ -10,11 +10,11 @@ function runSection {
       manualAction "Press Install Font Button for Inconsolata-g.otf"
 
       info "Opening Powerline Inconsolata-g font"
-      open "$(scriptDirectory)/fonts/'Inconsolata-g for Powerline.otf'"
+      open "$(scriptDirectory)/fonts/Inconsolata-g for Powerline.otf"
       manualAction "Press Install Font Button for Inconsolata-g for Powerline.otf"
 
       info "Opening Inconsolata Nerd Font Icons.otf"
-      open "$(scriptDirectory)/fonts/'Inconsolata Nerd Font Icons.otf'"
+      open "$(scriptDirectory)/fonts/Inconsolata Nerd Font Icons.otf"
       manualAction "Press Install Font Button for Inconsolata Nerd Font Icons.otf"
     else
       warn "This is a MacOS specific step, skipping due to invalid OS..."
